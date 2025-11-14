6ff52d10debc56fb64ee290283a75d525266d1f0
sssk9034 <seilakennedy.y@gmail.com>
2025-11-14 13:22:51 -0700

    navigation in header works as intended (for now)

---
+++ js/app.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const home = document.querySelector("#home");
+    const browse = document.querySelector("#browse");
+    const about = document.querySelector("#about");
+    const shoppingCart = document.querySelector("#shoppingCart");
+
+    document.querySelector("nav").addEventListener('click', (e) => {
+        if(e.target.nodeName == "IMG") {
+            showView("home")
+            return;
+        } else if (e.target.nodeName == "LI") {
+            const navList = document.querySelectorAll("#navigation li");
+            const clickedNav = e.target.textContent.trim();
+
+            if (clickedNav === "Home") {showView("home"); return;}
+            if (clickedNav === "Browse") {showView("browse"); return;}
+            if (clickedNav === "About") {document.querySelector("#about").showModal(); return;}
+        } else if (e.target.nodeName == "H3") {
+            showView("shoppingCart");
+        }
+    });
+
+    function showView(viewName) {
+        home.classList.add("hidden");
+        browse.classList.add("hidden");
+        shoppingCart.classList.add("hidden");
+
+        if (viewName === "home") home.classList.remove("hidden");
+        if (viewName === "browse") browse.classList.remove("hidden");
+        if (viewName === "shoppingCart") shoppingCart.classList.remove("hidden");
+    }
+});
