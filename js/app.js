2a6c38c0e56a31a467c63daf0c8aa12292e6f7ac
sssk9034 <seilakennedy.y@gmail.com>
2025-11-23 22:25:54 -0700

    added filters for browse page, still needs to be worked on

--- js/app.js
+++ js/app.js
@@ -40,4 +40,86 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#x").addEventListener('click', () => {
         document.querySelector("#about").close();
     });
+
+    // populate filter
+    // category
+    const categoryTemplate = document.querySelector("#category template");
+    const category = document.querySelector("#category");
+
+    const categories = [];
+    for (let product of products) {
+        if (!categories.includes(product.category)) {
+            categories.push(product.category);
+        }
+    }
+    categories.sort();
+    for (let c of categories) {
+        const clone = categoryTemplate.content.cloneNode(true);
+
+        const input = clone.querySelector("input");
+        input.setAttribute("id", c);
+        input.setAttribute("name", c);
+        input.setAttribute("value", c);
+        
+        const label = clone.querySelector("label");
+        label.textContent = c;
+
+        category.appendChild(clone);
+    }
+
+    // size **needs to be worked on still so it's in order
+    const sizeTemplate = document.querySelector("#size template");
+    const size = document.querySelector("#size");
+
+    const sizes = [];
+    for (let product of products) {
+        for (let s of product.sizes) {
+            if (!sizes.includes(s)) {
+                sizes.push(s)
+            }
+        }
+    }
+
+    for (let s of sizes) {
+        const clone = sizeTemplate.content.cloneNode(true);
+
+        const input = clone.querySelector("input");
+        input.setAttribute("id", s);
+        input.setAttribute("name", s);
+        input.setAttribute("value", s);
+        
+        const label = clone.querySelector("label");
+        label.textContent = s;
+
+        size.appendChild(clone);
+    }
+
+    // colors
+    const colorsTemplate = document.querySelector("#colors template");
+    const colorsDiv = document.querySelector("#colors");
+
+    const colors = [];
+    for (let product of products) {
+        for (let c of product.color) {
+            if (!colors.includes(c.name)) {
+            colors.push(c.name);
+        }
+        }
+    
+    }
+    colors.sort();
+    for (let c of colors) {
+        const clone = colorsTemplate.content.cloneNode(true);
+
+        const input = clone.querySelector("input");
+        input.setAttribute("id", c);
+        input.setAttribute("name", c);
+        input.setAttribute("value", c);
+        
+        const label = clone.querySelector("label");
+        label.textContent = c;
+
+        colorsDiv.appendChild(clone);
+    }
+
 });
